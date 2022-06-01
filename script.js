5ce0bf2ccbcdd2a6ff24bfb2f6a8c65c7ef217bb
WingHaa <quanlm24@gmail.com>
2022-06-01 16:57:36 -0700

    Add closing and opening logic for form modal

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ function addBookToLibrary(book) {
 Book.prototype.toggleRead = function (e) {
   return !e.read;
 }
+
+const addButton = document.querySelector('.add');
+addButton.addEventListener('pointerdown', toggleBookForm);
+
+const closeForm = document.querySelector('.exit');
+closeForm.addEventListener('pointerdown', toggleBookForm);
+
+function toggleBookForm() {
+  const bookForm = document.querySelector('.modal');
+  console.log(bookForm.style.display)
+  return bookForm.style.display === 'none' ? bookForm.style.display = 'flex' :
+    bookForm.style.display = 'none';
+}
+const submitBookDetail = document.querySelector('.submit')
+submitBookDetail.addEventListener('pointerdown', addBookToLib);
+
+function addBookToLib(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const formProps = Object.fromEntries(formData);
+  let book = new Book
+  myLibrary.push(book);
+}
+
+function showBookCard (book) {
+
+}
